275728ada1865a168576b14c6bd54db6e65bee62
kerri-wei <156868597+kerri-wei@users.noreply.github.com>
2024-03-26 20:44:10 -0400

    Update main.py

--- main.py
+++ main.py
@@ -11,11 +11,36 @@ This file is Copyright (c) Ashley Bi, Zhuoyi Jin, Elizabeth Liu, and Kerri Wei.
 """
 
 from Module(Data_Classes) import _Vertex, 
+import math
 
 if __name__ == "__main__":
     with open('Airline_Data.csv') as airline_file:
       flight_graph = Graph(airline_file)
 
-    print("Please type your current city:")
+    print("Please type your current city (please capitalize the first
+    letter and type the city name in full):")
+        location = input()
+
+    print("Please type your desired destination (please capitalize the first letter
+    and type the city name in full):")
+        destination = input()
+
+    print("Would you like to check bags on your flight? (Yes/No)")
+        bag_check = (input() == "Yes")
+
+    lower_price = -math.inf
+    while lower_price < 0:
+        print("Please type your lower price bound in CAD (please use numbers only):")
+            lower_price = int(input())
+            if lower_price < 0:
+                print("Please input a number that is greater than or equal to 0.")
+
+    upper_price = -math.inf
+     while upper_price < lower_price:
+        print("Please type your upper price bound in CAD (please use numbers only):")
+            upper_price = int(input())
+            if upper_price < lower_price:
+                print("Please input a number that is greater than or equal to your lower price bound.")
+
     
       
