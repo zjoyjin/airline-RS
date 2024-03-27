a2ac65b313a2807b7b1f620e6e7948ed391103e5
zjoyjin <joyee.jin1977@gmail.com>
2024-03-26 20:15:19 -0400

    basic functions

--- edges.py
+++ edges.py
@@ -1,3 +1,162 @@
 """
-This is all the Functions and classes
+This is the Classes and Function for Vertex and graph
 """
+
+from __future__ import annotations
+from typing import Any, Optional
+
+class _Vertex:
+    """A vertex in a graph.
+
+    Instance Attributes:
+        - item: The data stored in this vertex.
+        - neighbours: The vertices that are adjacent to this vertex.
+
+    Representation Invariants:
+        - self not in self.neighbours
+        - all(self in u.neighbours for u in self.neighbours)
+    """
+    item: Any
+    neighbours: set[_Vertex]
+
+    def __init__(self, item: Any, neighbours: set[_Vertex]) -> None:
+        """Initialize a new vertex with the given item and neighbours."""
+        self.item = item
+        self.neighbours = neighbours
+
+    def check_connected(self, target_item: Any, visited: set[_Vertex]) -> bool:
+        """Return whether this vertex is connected to a vertex corresponding to the target_item,
+        WITHOUT using any of the vertices in visited.
+
+        Preconditions:
+            - self not in visited
+        """
+        if self.item == target_item:
+            # Our base case: the target_item is the current vertex
+            return True
+        else:
+            visited.add(self)  # Add self to the set of visited vertices
+            for u in self.neighbours:
+                if u not in visited:  # Only recurse on vertices that haven't been visited
+                    if u.check_connected(target_item, visited):
+                        return True
+
+            return False
+
+    def check_connected_path(self, target_item: Any, visited: set[_Vertex]) -> Optional[list]:
+        """Return a path between self and the vertex corresponding to the target_item,
+        WITHOUT using any of the vertices in visited.
+
+
+        Preconditions:
+            - self not in visited
+
+        >>> v1 = _Vertex(1, set())
+        >>> v2 = _Vertex(2, set())
+        >>> v3 = _Vertex(3, set())
+        >>> v4 = _Vertex(4, set())
+        >>> v1.neighbours = {v2, v3}
+        >>> v2.neighbours = {v4}
+        >>> v1.check_connected_path(4, set())
+        [1, 2, 4]
+        >>> v1.check_connected_path(4, {v2}) is None
+        True
+
+        """
+
+        if self.item == target_item:
+            return [self.item]
+        else:
+            visited.add(self)  # Add self to the set of visited vertices
+            for u in self.neighbours:
+                if u not in visited:  # Only recurse on vertices that haven't been visited
+                    path = u.check_connected_path(target_item, visited)
+                    if path is not None:
+                        return [self.item] + path
+
+            return None
+
+
+class Graph:
+    """A graph.
+
+    Representation Invariants:
+        - all(item == self._vertices[item].item for item in self._vertices)
+    """
+    # Private Instance Attributes:
+    #     - _vertices:
+    #         A collection of the vertices contained in this graph.
+    #         Maps item to _Vertex object.
+    _vertices: dict[Any, _Vertex]
+
+    def __init__(self) -> None:
+        """Initialize an empty graph (no vertices or edges)."""
+        self._vertices = {}
+
+    def add_vertex(self, item: Any) -> None:
+        """Add a vertex with the given item to this graph.
+
+        The new vertex is not adjacent to any other vertices.
+
+        Preconditions:
+            - item not in self._vertices
+        """
+        if item not in self._vertices:
+            self._vertices[item] = _Vertex(item, set())
+
+    def add_edge(self, item1: Any, item2: Any) -> None:
+        """Add an edge between the two vertices with the given items in this graph.
+
+        Raise a ValueError if item1 or item2 do not appear as vertices in this graph.
+
+        Preconditions:
+            - item1 != item2
+        """
+        if item1 in self._vertices and item2 in self._vertices:
+            v1 = self._vertices[item1]
+            v2 = self._vertices[item2]
+
+            # Add the new edge
+            v1.neighbours.add(v2)
+            v2.neighbours.add(v1)
+        else:
+            # We didn't find an existing vertex for both items.
+            raise ValueError
+
+    def adjacent(self, item1: Any, item2: Any) -> bool:
+        """Return whether item1 and item2 are adjacent vertices in this graph.
+
+        Return False if item1 or item2 do not appear as vertices in this graph.
+        """
+        if item1 in self._vertices and item2 in self._vertices:
+            v1 = self._vertices[item1]
+            return any(v2.item == item2 for v2 in v1.neighbours)
+        else:
+            # We didn't find an existing vertex for both items.
+            return False
+
+    def connected(self, item1: Any, item2: Any) -> bool:
+        """Return whether item1 and item2 are connected vertices in this graph.
+
+        Return False if item1 or item2 do not appear as vertices in this graph.
+
+        >>> g = Graph()
+        >>> g.add_vertex(1)
+        >>> g.add_vertex(2)
+        >>> g.add_vertex(3)
+        >>> g.add_vertex(4)
+        >>> g.add_edge(1, 2)
+        >>> g.add_edge(2, 3)
+        >>> g.connected(1, 3)
+        True
+        >>> g.connected(1, 4)
+        False
+        """
+        if item1 in self._vertices and item2 in self._vertices:
+            v1 = self._vertices[item1]
+            return v1.check_connected(item2, set())  # Pass in an empty "visited" set
+        else:
+            return False
+
+
+
