6689889ace27b14e84f3e6400d62e3b6119f04f2
lizzie-liu <elizabethfj.liu@mail.utoronto.ca>
2024-03-28 15:47:40 -0400

    figured out how to get some of the info (need to import BeautifulSoup to help parse the html tho)

--- playwright/scrape.py
+++ playwright/scrape.py
@@ -59,3 +59,55 @@ def run(playwright):
 """running it (move to main eventually?)"""
 with sync_playwright() as playwright:
     run(playwright)
+
+
+# IT KINDA WORKS!!
+# from playwright.sync_api import sync_playwright
+# import time
+# from bs4 import BeautifulSoup
+#
+# with sync_playwright() as playwright:
+#     context = playwright.chromium.launch(headless=False).new_context()
+#     page = context.new_page()
+#     page.goto('https://www.google.com/travel/flights?hl=en-US&curr=CAD')
+#
+#     page.get_by_role("textbox", name="Departure").fill("2024/04/05")
+#     page.get_by_role("textbox", name="Return").fill("2024/04/28")
+#
+#     # type "From"
+#     from_place_field = page.locator('.e5F5td').first
+#     from_place_field.click()
+#     time.sleep(1)
+#     from_place_field.press_sequentially("Edmonton")
+#     time.sleep(1)
+#     page.keyboard.press('Enter')
+#
+#     # type "To"
+#     to_place_field = page.locator('.e5F5td').nth(1)
+#     to_place_field.click()
+#     time.sleep(1)
+#     to_place_field.press_sequentially("Vancouver")
+#     time.sleep(1)
+#     page.keyboard.press('Enter')
+#
+#     page.locator(".xFFcie").first.click()
+#
+#     time.sleep(1)
+#     soup = BeautifulSoup(page.content(), 'html.parser')
+#
+#     departures = soup.find_all('div', class_='wtdjmc YMlIz ogfYpf tPgKwe')
+#     arrivals = soup.find_all('div', class_='XWcVob YMlIz ogfYpf tPgKwe')
+#     airlines = soup.find_all('span', class_='h1fkLb')
+#     stops = soup.find_all('span', class_='VG3hNb')
+#
+#     parsed = {}
+#     for i in range(0, len(departures)):
+#         parsed[i] = []
+#
+#     for i in range(0, len(departures)):
+#         parsed[i].append(f'Departure: {departures[i].text}')
+#         parsed[i].append(f'Arrival: {arrivals[i].text}')
+#         parsed[i].append(f'Airline: {airlines[i].text}')
+#         parsed[i].append(f'# Stops: {stops[i].text}')
+#
+#     print(parsed)
