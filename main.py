55a97f5c431241a974cb9b91b4856b9d03eae92d
zjoyjin <joyee.jin1977@gmail.com>
2024-04-01 20:55:52 -0400

    update new graph

--- main.py
+++ main.py
@@ -7,71 +7,53 @@ Copyright and Usage Information
 This file is Copyright (c) Ashley Bi, Zhuoyi Jin, Elizabeth Liu, and Kerri Wei.
 """
 
-from Module(Data_Classes) import
-from edges import Vertex, Graph, Edge
+from flight_network_graph import Graph, Vertex
+from datetime
 import geoplotlib
 import math
 from typing import Any, Union
 
 
 if __name__ == "__main__":
-    with open('Airline_Data.csv') as airline_file:
-      flight_graph = Graph(airline_file)
+    #print cheap graph here
+    graph = Graph()
+    print("Please type your desired leaving date\n, in yyyy/mm/dd")
 
-    print("Please type your current city (please capitalize the first\n"
+    start_date = input()
+    graph.load_viewed_graph(start_date)
+
+
+    #read airport
+
+    print("Please type all of the airport city you want to go into\n"
           "letter and type the city name in full):")
-    location = input()
+    locations = input()
+    if locations not in airport.csv:
+        print("This is not a valid airport, retype")
+        locations = input()
 
-    print("Please type your desired destination (please capitalize the first letter\n"
-          "and type the city name in full):")
-    destination = input()
-
-    print("Would you like to check bags on your flight? (Yes/No)")
-    bag_check = (input() == "Yes")
-
-    upper_price = -math.inf
-    while upper_price <= 0:
-        print("Please type your upper price bound in CAD (please use numbers only):")
-        upper_price = int(input())
-        if upper_price <= 0:
-            print("Please input a number that is greater than or equal to $0.")
-
-    # Creating the graph with airline data
-    flight_graph = Graph('Airline_Data.csv')
-
-    matching_flights = []
-    for edge in flight_graph.edges[location]:
-        if edge.destination.name == destination and edge.baggage == ('yes' if bag_check else 'no') and 0 <= edge.price <= upper_price:
-            matching_flights.append(edge)
-
-    # Print out the matching flights
-    if matching_flights:
-        print("\nMatching flights:")
-        for flight in matching_flights:
-            print(flight)
-    else:
-        print("\nNo matching flights found.")
+    print("Please type your desired leaving date\n"
+          "type the format in yyyy/mm/dd):")
+    start_date = input()
 
 
-data = []
-for flight in matching_flights:
-    data.append({
-        'lat_departure': flight.start.lat,
-        'lon_departure': flight.start.lon,
-        'lat_arrival': flight.destination.lat,
-        'lon_arrival': flight.destination.lon
-    })
+    # # Creating the graph with airline data
+    # flight_graph = Graph('Airline_Data.csv')
+    #
+    # matching_flights = []
+    # for edge in flight_graph.edges[location]:
+    #     if edge.destination.name == destination and edge.baggage == ('yes' if bag_check else 'no') and 0 <= edge.price <= upper_price:
+    #         matching_flights.append(edge)
+    #
+    # # Print out the matching flights
+    # if matching_flights:
+    #     print("\nMatching flights:")
+    #     for flight in matching_flights:
+    #         print(flight)
+    # else:
+    #     print("\nNo matching flights found.")
 
 
 
-geoplotlib.graph(data,
-                 src_lat=’lat_departure’,
-                 src_lon=’lon_departure’,
-                 dest_lat=’lat_arrival’,
-                 dest_lon=’lon_arrival’,
-                 color=’hot_r’,
-                 alpha=16,
-                 linewidth=2)
-geoplotlib.show()
     
       
