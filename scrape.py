5aaa6166f26d0783b80d77aa60c59fa27c4887fa
lizzie-liu <elizabethfj.liu@mail.utoronto.ca>
2024-04-03 22:10:08 -0400

    getting rid of extra unused code

--- scrape.py
+++ scrape.py
@@ -53,9 +53,6 @@ def _get_results_page(page: Page, start: str, end: str, departure: str) -> str:
     page.locator(".xFFcie").first.click()
     sleep(2.5)
 
-    # page.locator(".zISZ5c").and_(page.locator(".QB2Jof")).click()   # get more flights
-    # sleep(2)    # if this doesn't work, then it'll just be as if this wasn't clicked
-
     return page.content()
 
 
@@ -66,43 +63,32 @@ def _parse(soup: BeautifulSoup) -> list[dict]:
         departure time, arrival time, airline, price (float),
         departure airport code, arrival airport code,
     """
-    # get info
+    # Get the required info
     departures = soup.find_all('div', class_='wtdjmc YMlIz ogfYpf tPgKwe')
     arrivals = soup.find_all('div', class_='XWcVob YMlIz ogfYpf tPgKwe')
     airlines = soup.find_all('span', class_='h1fkLb')
-    # stops = soup.find_all('span', class_='VG3hNb')
-    # airport_stops = soup.find_all('span', class_="rGRiKd")      # format: either "X stops in XYZ, ABC" or "Nonstop"
     prices = soup.find_all('div', class_="BVAVmf I11szd POX3ye")
     airportstart = soup.find_all('div', class_="G2WY5c sSHqwe ogfYpf tPgKwe")
     airportend = soup.find_all('div', class_="c8rWCd sSHqwe ogfYpf tPgKwe")
-    # durations = soup.find_all('span', class_="EzfXjb")
-    # baggage = soup.find_all('div', class_='BVAVmf I11szd POX3ye')
 
-    # return info as list of dictionaries
+    # Return info as list of dictionaries
     results = []
     for i in range(0, len(departures)):
         results.append({'Departure': None,
                         'Arrival': None,
                         'Airline': None,
                         'Price': None,
-                        # 'Duration': None,
                         'From': None,
-                        'To': None,
-                        # 'Stops': None,
-                        # 'Overhead': None
+                        'To': None
                         })
 
     for i in range(0, len(departures)):
         results[i]['Departure'] = departures[i].text.replace("\u202f", " ")
         results[i]['Arrival'] = arrivals[i].text.replace("\u202f", " ")
         results[i]['Airline'] = airlines[i].text
-        # results[i].append(f'# Stops: {stops[i].text}')
         results[i]['Price'] = float([p for p in prices[i].descendants][-1][3:].replace(',', ''))  # int
-        # results[i]['Duration'] = durations[i].find_previous_sibling().text
         results[i]['From'] = airportstart[i].text
         results[i]['To'] = airportend[i].text
-        # results[i]['Stops'] = airport_stops[i].text
-        # results[i]['Overhead'] = baggage[i].find('svg') is None     # bool
 
     return results
 
@@ -124,11 +110,9 @@ def get_results(start: str, end: str, departure: str) -> list[dict]:
     """
 
     with sync_playwright() as playwright:
-        browser = playwright.chromium.launch(headless=False)     # Set to False to see Chromium pop-up
+        browser = playwright.chromium.launch(headless=False)  # Set to False to see Chromium pop-up
         page = browser.new_page()
         page.goto('https://www.google.com/travel/flights?hl=en-US&curr=CAD')
-        # could probably get currency customization by changing curr=   ^
-        # but then would need to alter how price str -> int is done
 
         print("Getting results...")
 
@@ -140,7 +124,7 @@ def get_results(start: str, end: str, departure: str) -> list[dict]:
 
 # For testing purposes
 if __name__ == "__main__":
-    res = get_results("Vancouver", "Edmonton", "2024/04/20")
+    res = get_results("Vancouver", "Edmonton", "2024/20/04")
     if res:
         print(res)
     else:
